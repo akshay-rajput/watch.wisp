2f2343ea26ab89ecb36b76f935466c82f6eec2b4
Akshay <akshayrajput102@gmail.com>
2021-05-16 12:28:59 +0530

    feat - navUserOptions - v1 added

---
+++ src/Components/common/NavUserOptions.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './NavUserOptions.module.css';
+
+export default function NavUserOptions() {
+    return (
+        <div className={styles.user_options_bottom_drawer}>
+            here is the list of options...
+        </div>
+    )
+}
