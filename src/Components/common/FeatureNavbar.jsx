08d42fe24d8f40607cc8186a77aff3f32a8f0df7
Akshay <akshayrajput102@gmail.com>
2021-05-16 22:40:25 +0530

    feat - featureNavbar - use navlink to route

--- src/Components/common/FeatureNavbar.jsx
+++ src/Components/common/FeatureNavbar.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import styles from './FeatureNavbar.module.css';
-import {MdHome, MdAddToPhotos, MdVideoLibrary, MdBookmarkBorder} from 'react-icons/md'
+import {NavLink} from 'react-router-dom';
+import {MdHome, MdAddToPhotos, MdVideoLibrary, MdBookmark} from 'react-icons/md'
 
 export default function FeatureNavbar() {
     return (
         <div className={`${styles.feature_navbar}`}>
-            <button className={`${styles.active_link} borderNone textGray3 bgTransparent displayFlex flexCol itemsCenter md:mb8`}>
+            <NavLink end activeClassName="active_link" to="/" className={`${styles.nav_link} borderNone  bgTransparent displayFlex flexCol itemsCenter`}>
                 <MdHome className="textLg" />
                 <span className="textXs pt2">Home</span>
-            </button>
-            <button className={` borderNone textGray3 bgTransparent displayFlex flexCol itemsCenter md:mb8`}>
+            </NavLink>
+
+            <NavLink end activeClassName="active_link" to="/addVideo" className={` ${styles.nav_link} borderNone  bgTransparent displayFlex flexCol itemsCenter`}>
                 <MdAddToPhotos className="textLg"/>
                 <span className="textXs pt2">Add Video</span>
-            </button>
-            <button className={` borderNone textGray3 bgTransparent displayFlex flexCol itemsCenter md:mb8`}>
-                <MdBookmarkBorder className="textLg" />
+            </NavLink>
+            
+            <NavLink end activeClassName="active_link" to="/playlists/savedVideos" className={` ${styles.nav_link} borderNone  bgTransparent displayFlex flexCol itemsCenter`}>
+                <MdBookmark className="textLg" />
                 <span className="textXs pt2">Saved Videos</span>
 
-            </button>
-            <button className={` borderNone textGray3 bgTransparent displayFlex flexCol itemsCenter md:mb8`}>
+            </NavLink>
+            
+            <NavLink end activeClassName="active_link" to="/playlists" className={` ${styles.nav_link} borderNone  bgTransparent displayFlex flexCol itemsCenter`}>
                 <MdVideoLibrary className="textLg" /> 
                 <span className="textXs pt2">Playlists</span>
-            </button>
+            </NavLink>
         </div>
     )
 }
