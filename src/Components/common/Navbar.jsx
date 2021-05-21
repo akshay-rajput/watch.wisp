af83d011e36959609bb4ce31f95633c0d08a6b73
Akshay <akshayrajput102@gmail.com>
2021-05-21 12:19:11 +0530

    feat - navbar - add authState, conditional links

--- src/Components/common/Navbar.jsx
+++ src/Components/common/Navbar.jsx
@@ -1,10 +1,14 @@
-import React, {useState} from 'react'
-import styles from './Navbar.module.css'
-import {MdExpandMore, MdExpandLess} from 'react-icons/md'
-import NavUserOptions from './NavUserOptions'
-import wisplogo from '../../../wisp.png'
+import React, {useState, useEffect} from 'react';
+import styles from './Navbar.module.css';
+import {NavLink} from 'react-router-dom';
+import {MdExpandMore, MdExpandLess, MdSearch} from 'react-icons/md'
+import NavUserOptions from './NavUserOptions';
 
-export default function Navbar() {
+import {useAuth} from '../../Store/AuthContext';
+
+import wisplogo from '../../../wisp.png';
+
+export default function Navbar({isUserLoggedIn, username}) {
     const [showOptions, setShowOptions] = useState(false);
 
     function toggleUserOptions(){
@@ -12,38 +16,51 @@ export default function Navbar() {
         setShowOptions(!showOptions);
     }
 
+
     return (
         <nav className={`${styles.navbar}`}>
             <div className={`${styles.nav_container} displayFlex justifyBetween itemsCenter p3 md:pl4 md:pr4 `}>
-                <a href="#" className={`${styles.nav_brand} logofont displayFlex itemsCenter`}>
+                {/* <a href="#" className={`${styles.nav_brand} logofont displayFlex itemsCenter`}>
                     <img src={wisplogo} alt="Wisp" className="h8 mr2"/>
                     Wisp
-                </a>
+                </a> */}
+                <NavLink to="/" className={`${styles.nav_brand} logofont displayFlex itemsCenter`}>
+                    <img src={wisplogo} alt="Wisp" className="h8 mr2"/>
+                    Wisp
+                </NavLink>
 
-                <div className="displayFlex itemsCenter listNoStyle flexWrap">
-                    
-                    <input type="search" autoComplete='text' placeholder="Search..." className="textWhite p2 mr6 md:mr10 textRg rounded bgTransparent border borderGray4" required/>
-                        
-                    <a href="" className="mr4 ">Login</a>
-                    <a href="" className=" ">Signup</a>
-                    
-                    <div className={`${styles.user_info} displayFlex itemsCenter textGray2`}>
-                        <div className="avatar-wrapper displayFlex itemsCenter mr2 bgGray6 w10 h10 roundedFull p1">
-                            <img src="https://avatars.dicebear.com/api/avataaars/124562.svg" className="roundedFull wFull" />
+                <div className="displayFlex itemsCenter listNoStyle">
+                    {   !isUserLoggedIn &&
+                        <div className="">
+                            <NavLink to="/login">
+                                Login
+                            </NavLink>
+                            <NavLink to="/signup" className="ml3">
+                                Signup
+                            </NavLink>
                         </div>
-                        
-                        <button onClick={toggleUserOptions} className="cursorPointer bgTransparent textRg textGray2 borderNone displayFlex itemsCenter">
-                            {"Akshay Rajput"}
-                            {!showOptions && <MdExpandMore className="textLg" />}
-                            {showOptions && <MdExpandLess className="textLg" />}
-                        </button>
-                        
-                    </div>
+                    }
+                    
+                    {
+                        isUserLoggedIn && 
+                        <div onClick={toggleUserOptions} className={`${styles.user_info} cursorPointer displayFlex itemsCenter textGray2`}>
+                            <div className="avatar-wrapper displayFlex itemsCenter bgGray6 w10 h10 roundedFull p1">
+                                <img src="https://avatars.dicebear.com/api/bottts/124562.svg" className="roundedFull wFull" />
+                            </div>
+                            
+                            <button className="cursorPointer bgTransparent textRg textGray2 borderNone displayFlex itemsCenter">
+                                <span className={`${styles.nav_username} ml2 mr1`}>{username}</span>
+                                {!showOptions && <MdExpandMore className="textLg" />}
+                                {showOptions && <MdExpandLess className="textLg" />}
+                            </button>
+                            
+                        </div>
+                    }
                     
                 </div> 
 
                 {  /* show dropdown of user options */
-                    showOptions && <NavUserOptions />
+                    showOptions && <NavUserOptions setShowOptions={setShowOptions}/>
                 }   
             </div>
         </nav>
