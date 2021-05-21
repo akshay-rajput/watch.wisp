3e5cb5049c6022aae10e3eefb14cfb7fb19d2700
Akshay <akshayrajput102@gmail.com>
2021-05-21 12:27:30 +0530

    feat - navUserOptions - add logout functionality

--- src/Components/common/NavUserOptions.jsx
+++ src/Components/common/NavUserOptions.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import styles from './NavUserOptions.module.css';
+import {MdExitToApp, MdSettings} from 'react-icons/md';
+import {useAuth} from '../../Store/AuthContext';
+import useLocalStorage from '../../Hooks/useLocalStorage';
 
-export default function NavUserOptions() {
+export default function NavUserOptions({setShowOptions}) {
+    const {authState, authDispatch} = useAuth();
+    
+    function logOutUser(){
+        setShowOptions(false);
+        authDispatch({ type: 'LOGOUT'});
+    }
+
     return (
-        <div className={styles.user_options_bottom_drawer}>
-            here is the list of options...
+        <div className={`${styles.user_options_bottom_drawer} displayFlex flexCol`}>
+            <span className={`${styles.user_options} displayFlex itemsCenter p2 `}>
+                <MdSettings className="mr1 mt1" />
+                Account
+            </span>
+            <span onClick={logOutUser} className={`${styles.user_options} displayFlex itemsCenter p2`}>
+                <MdExitToApp className="mr1 mt1" />
+                Logout
+            </span>
         </div>
     )
 }
