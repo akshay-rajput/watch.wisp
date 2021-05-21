183ea2f4c8d77e7c9432ff2c372f40fc65151142
Akshay <akshayrajput102@gmail.com>
2021-05-21 12:29:08 +0530

    feat - Login - add dispatch, useLocalStorage on successful login

--- src/Views/Login.jsx
+++ src/Views/Login.jsx
@@ -1,28 +1,97 @@
-import React, {useState, useRef} from 'react'
+import React, {useState, useContext, useRef} from 'react'
 import styles from './Login.module.css';
 import {MdVisibility, MdVisibilityOff} from 'react-icons/md';
 import {Link} from 'react-router-dom';
 
+import axios from 'axios';
+import useLocalStorage from '../Hooks/useLocalStorage';
+import {useAuth} from '../Store/AuthContext';
+
 export default function Login() {
+    const {authState, authDispatch} = useAuth();
+    
+    const [userId, setUserId] = useLocalStorage("userId", "");
+    const [username, setUsername] = useLocalStorage("username", "");
+    const [token, setToken] = useLocalStorage("token", null);
+
+    const initialState = {
+        email: '',
+        password: '',
+        isSubmitting: false,
+        errorMessage: null
+    }
+    const [loginData, setLoginData] = useState(initialState);
+
     const [showPassword, setShowPassword] = useState(false);
     const password = useRef(null);
 
-    function doLogin(event){
+    async function doLogin(event){
         event.preventDefault();
-    }
+        console.log('login: ', loginData);
+        // loading
+        setLoginData({
+            ...loginData,
+            isSubmitting: true,
+            errorMessage: null
+        })
 
-    function togglePasswordVisibility(){
-        
-        if(showPassword){
-            password.current.type = 'text';
-        }else{
-            password.current.type = 'password';
+        // make a request and dispatch
+        try{
+            const response = await axios.post("https://watch-wisp.herokuapp.com/login", loginData);
+            console.log('login response: ', response);
+
+            let userData = {
+                name: response.data.user.name,
+                id: response.data.user._id,
+            }
+
+            authDispatch({
+                type: 'LOGIN',
+                payload: userData
+            })
+
+            // store in local
+            setUserId(userData.id);
+            setUsername(userData.name);
+            setToken(userData.id);
+
+            // finish loading
+            setLoginData({
+                ...loginData,
+                isSubmitting: false,
+                errorMessage: null
+            })
+
+        }
+        catch(error){
+            console.log('Error signup: ', error);
+
+            // finish loading
+            setSignupData({
+                ...signupData,
+                isSubmitting: false,
+                errorMessage: error
+            })
         }
 
-        setShowPassword(showPassword => !showPassword);
-        
-        
     }
+
+    function handleInputChange(event){
+        setLoginData({
+            ...loginData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    // function togglePasswordVisibility(){
+    //     setShowPassword(showPassword => !showPassword);
+        
+    //     if(showPassword){
+    //         password.current.type = 'text';
+    //     }else{
+    //         password.current.type = 'password';
+    //     }
+    // }
 
     return (
         <div className={styles.login_form + " border borderGray2"}>
@@ -32,14 +101,17 @@ export default function Login() {
             <form onSubmit={doLogin}>
                 <div className={"displayFlex flexCol pl4 pr4 mb4"}>
                     <label htmlFor="user_email" className="textSm textGray4 mb1">Email</label>
-                    <input type="email" id="user_email" autoComplete="true" placeholder="abc@example.com" className={styles.form_input}/>
+                    <input type="email" id="user_email" name="email" value={loginData.email} onChange={handleInputChange}
+                        autoComplete="true" placeholder="abc@example.com" className={styles.form_input}/>
                 </div>
 
                 <div className={"displayFlex flexCol pl4 pr4 mb4"}>
                     <label htmlFor="user_password" className="textSm textGray4 mb1">Password</label>
                     <div className="" style={{position: 'relative'}}>
-                        <input type="password" id="user_password" ref={password} placeholder="Enter Password" className={styles.form_input + " wFull"}/>
-                        <span onClick={togglePasswordVisibility} className={styles.password_visibility_toggler + " cursorPointer displayBlock textGray4"}>
+                        <input type={showPassword ? "text":"password"} id="user_password" name='password' ref={password}  onChange={handleInputChange}
+                                placeholder="Enter Password" className={styles.form_input + " wFull"}/>
+
+                        <span onClick={() => setShowPassword(!showPassword)} className={styles.password_visibility_toggler + " cursorPointer displayBlock textGray4"}>
                             {
                                 showPassword ? <MdVisibility /> : <MdVisibilityOff /> 
                             }
@@ -49,7 +121,7 @@ export default function Login() {
 
                 <div className="p4 mt8">
                     <button className="displayBlock wFull p3 textRg bgBlue4 textWhite borderNone rounded">
-                        Login
+                        { loginData.isSubmitting? 'loading..':'Login'}
                     </button>
 
                     <p className="mt8 textCenter textGray4">
