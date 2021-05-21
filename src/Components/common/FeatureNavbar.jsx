051907abb2541d3266f7a90724f23d9d75801950
Akshay <akshayrajput102@gmail.com>
2021-05-21 12:17:57 +0530

    Feat - sideNav - add navlinks and styles

--- src/Components/common/FeatureNavbar.jsx
+++ src/Components/common/FeatureNavbar.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
 import styles from './FeatureNavbar.module.css';
 import {NavLink} from 'react-router-dom';
-import {MdHome, MdAddToPhotos, MdVideoLibrary, MdBookmark} from 'react-icons/md'
+import {MdHome, MdAddToPhotos, MdVideoLibrary, MdBookmark, MdSearch} from 'react-icons/md'
 
 export default function FeatureNavbar() {
     return (
         <div className={`${styles.feature_navbar}`}>
-            <NavLink end activeClassName="active_link" to="/" className={`${styles.nav_link} borderNone  bgTransparent displayFlex flexCol itemsCenter`}>
+            <NavLink end activeClassName="active_link" to="/" className={`${styles.nav_link} borderNone  bgTransparent`}>
                 <MdHome className="textLg" />
-                <span className="textXs pt2">Home</span>
+                <span className={`${styles.nav_label} pt2`}>Home</span>
             </NavLink>
 
-            <NavLink end activeClassName="active_link" to="/addVideo" className={` ${styles.nav_link} borderNone  bgTransparent displayFlex flexCol itemsCenter`}>
+            <NavLink end activeClassName="active_link" to="/search" className={` ${styles.nav_link} borderNone  bgTransparent`}>
+                <MdSearch className="textLg"/>
+                <span className={`${styles.nav_label} pt2`}>Search</span>
+            </NavLink>
+
+            <NavLink end activeClassName="active_link" to="/addVideo" className={` ${styles.nav_link} borderNone  bgTransparent`}>
                 <MdAddToPhotos className="textLg"/>
-                <span className="textXs pt2">Add Video</span>
+                <span className={`${styles.nav_label} pt2`}>Add Video</span>
             </NavLink>
             
-            <NavLink end activeClassName="active_link" to="/playlists/savedVideos" className={` ${styles.nav_link} borderNone  bgTransparent displayFlex flexCol itemsCenter`}>
+            <NavLink end activeClassName="active_link" to="/playlists/savedVideos" className={` ${styles.nav_link} borderNone  bgTransparent`}>
                 <MdBookmark className="textLg" />
-                <span className="textXs pt2">Saved Videos</span>
+                <span className={`${styles.nav_label} pt2`}>Saved Videos</span>
 
             </NavLink>
             
-            <NavLink end activeClassName="active_link" to="/playlists" className={` ${styles.nav_link} borderNone  bgTransparent displayFlex flexCol itemsCenter`}>
+            <NavLink end activeClassName="active_link" to="/playlists" className={` ${styles.nav_link} borderNone  bgTransparent`}>
                 <MdVideoLibrary className="textLg" /> 
-                <span className="textXs pt2">Playlists</span>
+                <span className={`${styles.nav_label} pt2`}>Playlists</span>
             </NavLink>
         </div>
     )
