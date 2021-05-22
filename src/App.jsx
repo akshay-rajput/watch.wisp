7ddf6b92df5e2243e5868a8f66541ee76df1bc93
Akshay <akshayrajput102@gmail.com>
2021-05-22 15:34:54 +0530

    feat - app - render routes + some old code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,68 @@
-import React, { useState } from 'react'
-// import logo from './logo.svg'
-import './App.css'
+import React, { useState, useContext, useEffect } from "react";
+import ReactPlayer from "react-player/youtube";
+import axios from "axios";
+import useWindowDimensions from "./Hooks/useWindowDimensions";
+import {useAuth} from './Store/AuthContext';
+
+import AppNavbar from "./Components/common/Navbar";
+import FeatureNavbar from './Components/common/FeatureNavbar';
+
+// render routes
+import ROUTES, {RenderRoutes} from './routes';
+
+import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      
-    </div>
-  )
+    // const [videosJson, setvideosJson] = useState([]);
+    const { height, width } = useWindowDimensions();
+
+    function resetVideos() {
+        setvideos([]);
+    }
+
+    // authentication
+    const {authState, authDispatch} = useAuth();
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+    useEffect(() => {
+        console.log('init navbar: ', authState.token);
+
+        if(authState.token){
+            setIsUserLoggedIn(true);
+        }else{
+            setIsUserLoggedIn(false);
+        }
+        
+    }, [authState.token])
+
+    return (
+        <div className="App textGray6">
+            <AppNavbar isUserLoggedIn={isUserLoggedIn} username={authState.username}/>
+
+            <div className="app-main">
+				{/* sidebar or bottom nav */}
+				<FeatureNavbar />
+
+				{/* main content */}
+				<div className="app-content pl3 pr3">
+                    {/* route */}
+                    <RenderRoutes routes={ROUTES}></RenderRoutes>
+                </div>
+			</div>
+
+            {/* other test code */}
+            <div className="mt7">
+                {/* <span></span> */}
+
+                {/* <div className="">
+						<ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' controls={true} width={width-32} height={width * 0.5625} style={{margin:'auto'}}/>
+					</div> */}
+
+                {/* <span></span> */}
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
