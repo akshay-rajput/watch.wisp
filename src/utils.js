07bea2267929ba36a0773ec6ccce1196c8434cbf
Akshay <akshayrajput102@gmail.com>
2021-05-25 22:38:30 +0530

    refactor - utils - checkExistance check for type of array element

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,33 @@
 export const checkExistanceInArray = (array, id) => {
     // returns true or false
+    if(typeof array[0] == 'string'){
+        return !!array.find((item) => item === id);
+    }
     return !!array.find((item) => item._id === id);
 };
+
+// truncate text to character limit
+export const truncateText = (text, numberOfCharacters) =>{
+    if(text.length >= numberOfCharacters){
+        return text.substring(0,numberOfCharacters)+'...';
+    }
+    return text;
+}
+
+// convert youtube video duration
+export const convertDuration = (duration) => {
+    let modifiedDuration = '';
+
+    // if no seconds provided add 00 secs
+    if(duration[duration.length - 1] === "M"){
+        modifiedDuration = duration.replace("PT","").replace("H",":").replace("M",":")+"00";
+    }
+    else{
+        modifiedDuration = duration.replace("PT","").replace("H",":").replace("M",":").replace("S","");
+    }
+    // console.log('modified: ', modifiedDuration);
+    return modifiedDuration;
+}
 
 // convert number
 export const convertNumberScale = (number) => {
