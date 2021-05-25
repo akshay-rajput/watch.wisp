513520d24dbc18ecf9eaa059aeac48cac4d523f8
Akshay <akshayrajput102@gmail.com>
2021-05-25 23:09:42 +0530

    feat - context for user playlists

---
+++ src/Store/PlaylistsContext.jsx
@@ -0,0 +1,17 @@
+import React,{ createContext, useContext, useReducer } from "react";
+import PlaylistsReducer, {initialState} from './PlaylistsReducer';
+
+const PlaylistsContext = createContext({});
+
+export const usePlaylists = () => {
+    return useContext(PlaylistsContext);
+}
+
+export function PlaylistsProvider({children}) {
+
+    const [playlistsState, playlistsDispatch] = useReducer(PlaylistsReducer, initialState);
+    console.count('inside playlist context..')
+    return <PlaylistsContext.Provider value = {{ playlistsState, playlistsDispatch }}>
+        {children}
+    </PlaylistsContext.Provider>
+}
