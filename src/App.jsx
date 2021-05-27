f6a983f9190a9dd65c137cd9f3149b8a639750c4
Akshay <akshayrajput102@gmail.com>
2021-05-27 17:07:23 +0530

    feat - App - dispatch getUserPlaylists, set savedVideos, likedVideos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ReactPlayer from "react-player/youtube";
 import axios from "axios";
 import useWindowDimensions from "./Hooks/useWindowDimensions";
 import {useAuth} from './Store/AuthContext';
-
+import {usePlaylists} from './Store/PlaylistsContext';
 import AppNavbar from "./Components/common/Navbar";
 import FeatureNavbar from './Components/common/FeatureNavbar';
 
@@ -17,16 +17,15 @@ function App() {
     // const [videosJson, setvideosJson] = useState([]);
     const { height, width } = useWindowDimensions();
 
-    function resetVideos() {
-        setvideos([]);
-    }
-
     // authentication
     const {authState, authDispatch} = useAuth();
+    const {playlistsState, playlistsDispatch} = usePlaylists();
+    
     const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
+    // auth
     useEffect(() => {
-        console.log('init navbar: ', authState.token);
+        console.log('app token: ', authState.token);
 
         if(authState.token){
             setIsUserLoggedIn(true);
@@ -35,6 +34,59 @@ function App() {
         }
         
     }, [authState.token])
+
+    // user playlists
+    useEffect(() => {    
+
+        if(authState.userId){
+            (async function getUserPlaylists(){
+                try{
+                    let response = await axios.get('https://watch-wisp.herokuapp.com/playlists', {
+                        headers: {
+                            userId: authState.userId
+                        }
+                    })  
+                    let userPlaylists = response.data.userPlaylists;
+                    console.log('response of getUserPlaylist: ', userPlaylists)
+                    
+                    let savedVideos = {};
+                    userPlaylists.map(playlist => {
+                        if(playlist.name === "Saved Videos"){
+                            savedVideos = {...playlist};
+                        }
+                    })
+                    console.log('saved: ', savedVideos);
+                    let likedVideos = {};
+                    likedVideos = userPlaylists.map(playlist => {
+                        if(playlist.name === "Liked Videos"){
+                            return playlist;
+                        }
+                    })
+
+                    playlistsDispatch(
+                        {
+                            type: "GET_ALL_PLAYLISTS",
+                            payload: {
+                                userId: authState.userId,
+                                playlists: userPlaylists,
+                                savedVideos: savedVideos,
+                                likedVideos: likedVideos
+                            }
+                        }
+                    )
+                }
+                catch(error){
+                    console.log('error gettingUserPlaylsit: ', error);
+                }
+            })()            
+        }
+        else{
+            playlistsDispatch({type: "RESET_PLAYLISTS"});
+
+            console.log('user login required to fetch playlists...');
+        }
+            
+    }, [authState.userId]);
 
     return (
         <div className="App textGray6">
