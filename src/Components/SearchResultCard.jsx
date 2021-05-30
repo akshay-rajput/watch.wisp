83d78c790cad678f282f437eced57c93b62884c5
Akshay <akshayrajput102@gmail.com>
2021-05-30 22:52:11 +0530

    feat - searchResultCard - add playlist actions

--- src/Components/SearchResultCard.jsx
+++ src/Components/SearchResultCard.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './SearchResultCard.module.css';
 import {MdVisibility, MdThumbUp, MdBookmark, MdBookmarkBorder, MdPlaylistAdd} from 'react-icons/md';
 import {convertNumberScale} from '../utils';
+import {Link} from 'react-router-dom';
+
+import SaveVideoButton from './common/SaveVideoButton';
+import PlaylistPopup from './common/PlaylistPopup';
 
 export default function SearchResultCard({video}) {
+    const [isPlaylistPopupOpen, setIsPlaylistPopupOpen] = useState(false)
+
+    function openPlaylistPopup(){
+        setIsPlaylistPopupOpen(!isPlaylistPopupOpen);
+    }
+
+    function closePopup(){
+        setIsPlaylistPopupOpen(false);
+    }
 
     return (
         <div className={`${styles.result_card} gridColSpan2 md:gridColSpan1`}>
@@ -11,31 +24,33 @@ export default function SearchResultCard({video}) {
             
             <div className={`${styles.result_card_info} displayFlex flexCol`}>
                 <div className="flexGrow">
-                    <h4 className={`${styles.result_card_title} md:mb4 fontNormal`}>
+                    <Link to={`/video/${video._id}`} className={`${styles.result_card_title} displayBlock md:mb4 fontNormal`}>
                         {video.snippet.name}
-                    </h4>
+                    </Link>
 
                     <div className="result_card_stats displayFlex itemsCenter mt1">
-                        <span className="textXs md:textSm mr4 displayFlex itemsCenter">
-                            <MdVisibility className="mr1" /> {convertNumberScale(video.views)}
+                        <span className="textXs textGray4 md:textSm mr4 displayFlex itemsCenter">
+                            <MdVisibility className="mr1 " /> {convertNumberScale(video.views)}
                         </span>
-                        <span className="textXs md:textSm displayFlex itemsCenter">
+                        <span className="textXs textGray4 md:textSm displayFlex itemsCenter">
                             <MdThumbUp className="mr1" /> {convertNumberScale(video.likes)}
                         </span>
                     </div>
                 </div>
 
                 <div className="result_card_actions displayFlex itemsCenter" style={{justifyContent: 'flex-end'}}>
-                    <button className={`${styles.result_card_action} rounded ml2 mr2`}>
-                        <MdBookmark />
-                        <MdBookmarkBorder />
+                    
+                    <SaveVideoButton video={video} />
+
+                    <button onClick={openPlaylistPopup} className={`${styles.result_card_action} rounded`} title={'Add to playlist'}>
+                        <MdPlaylistAdd />    
                     </button>
 
-                    <button className={`${styles.result_card_action} rounded`}>
-                        <MdPlaylistAdd />
-                    </button>
                 </div>
 
+                {   isPlaylistPopupOpen &&
+                    <PlaylistPopup isOpen={isPlaylistPopupOpen} videoData={video} closePopup = {closePopup} />
+                }
             </div>
             
         </div>
