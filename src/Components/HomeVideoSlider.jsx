e31ad42baaf738540cad8e38660cebf2cb24e554
Akshay <akshayrajput102@gmail.com>
2021-05-31 20:14:03 +0530

    feat - HomeVideoSlider - add link to view all

--- src/Components/HomeVideoSlider.jsx
+++ src/Components/HomeVideoSlider.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import {MdArrowForward} from 'react-icons/md';
+import {MdChevronRight} from 'react-icons/md';
+import {Link} from 'react-router-dom';
 import HomeVideoCard from './HomeVideoCard';
 import styles from './HomeVideoSlider.module.css';
 
 export default function HomeVideoSlider({videoList, title}) {
     return (
-        <div >
-            <div className="displayFlex justifyBetween itemsCenter pl2 pr2">
+        <div className="mb12">
+            <div className="displayFlex justifyBetween itemsCenter pl2 pr2 mb2">
                 <div className="displayFlex itemsCenter">
                     <h4 className="textMd textGray5 mr1">{title} </h4>
                     {/* <span className="textSm textGray4">( {videoList.length} )</span> */}
                 </div>
-                <a href="" className="">
-                    <MdArrowForward /> View All
-                </a>
+                <Link to={title !== "Pronunciation" && title !=="Vocabulary" ? "category/hindi": "category/"+title} className=" displayFlex itemsCenter">
+                    View All <MdChevronRight />
+                </Link>
             </div>
             
             <div className={`${styles.video_slider}`}>
