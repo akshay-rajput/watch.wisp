82ce60b13a7a513ac233d89789a3b04c0fb61aba
Akshay <akshayrajput102@gmail.com>
2021-05-31 20:13:20 +0530

    feat - homeVideoCard - add link to video

--- src/Components/HomeVideoCard.jsx
+++ src/Components/HomeVideoCard.jsx
@@ -4,11 +4,14 @@ import {MdPlaylistAdd} from 'react-icons/md';
 import {convertNumberScale, truncateText, convertDuration, checkExistanceInArray} from '../utils';
 import PlaylistPopup from '../Components/common/PlaylistPopup';
 import SaveVideoButton from '../Components/common/SaveVideoButton';
+import {Link, useLocation} from 'react-router-dom';
 
 import useWindowDimensions from "../Hooks/useWindowDimensions";
 
 export default function HomeVideoCard({video}) {
     const {width} = useWindowDimensions();
+
+    const {pathname} = useLocation();
 
     const [isPlaylistPopupOpen, setIsPlaylistPopupOpen] = useState(false)
 
@@ -21,20 +24,24 @@ export default function HomeVideoCard({video}) {
     }
 
     return (
-        <div className={`${styles.video_card}`}>
-            <div className={`${styles.video_card_imagewrap}`}>
+        <div className={pathname=='/' ? styles.video_card : styles.category_video_card}>
+            {/* <div className={`${styles.video_card_imagewrap}`}>
                 <img src={video.snippet.thumbnailUrl} alt="Video Thumbnail" title={video.snippet.name} className={`${styles.video_card_image}`} />
                 <span className={`${styles.video_card_duration} textSm`}>{convertDuration(video.duration)}</span>
 
-            </div>
+            </div> */}
+            <Link to={'/video/'+video._id} title={video.snippet.name} className={`${styles.video_card_imagewrap}`}>
+                <img src={video.snippet.thumbnailUrl} alt="Video Thumbnail" title={video.snippet.name} className={`${styles.video_card_image}`} />
+                <span className={`${styles.video_card_duration} textSm`}>{convertDuration(video.duration)}</span>
+            </Link>
             <div className={`pl2 pr2 pb2`}>
-                <h4 title={video.snippet.name} className={`${styles.video_card_title} cursorPointer fontNormal`}>{video.snippet.name}</h4>
-
+                <Link to={'/video/'+video._id} title={video.snippet.name} className={`${styles.video_card_title} cursorPointer fontNormal`}>
+                    {video.snippet.name}
+                </Link>
                 <span className="textXs textGray4">
                     {width < 768 ? truncateText(video.channelTitle, 25) : truncateText(video.channelTitle, 40)} &bull; {convertNumberScale(video.views)} views
                 </span>
 
-                
                 {/* add to playlist */}
                 <div className={`${styles.video_card_button_group} mt2`}>
                     
