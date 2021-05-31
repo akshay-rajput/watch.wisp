28f3946b137dae58a2923336832829ed5b19760d
Akshay <akshayrajput102@gmail.com>
2021-05-31 19:35:16 +0530

    feat - category pages added

---
+++ src/Views/HindiVideos.jsx
@@ -0,0 +1,55 @@
+import React, {useEffect} from 'react';
+import HomeVideoCard from '../Components/HomeVideoCard';
+
+import {
+    getVideos,
+    hindiVideos,
+    vocabularyVideos,
+    pronunciationVideos
+} from '../getVideos';
+
+import {usePlaylists} from '../Store/PlaylistsContext';
+
+export default function HindiVideos() {
+    const {playlistsState, playlistsDispatch} = usePlaylists();
+
+    useEffect(() => {
+        // make request only if playlist is empty in state
+        if(playlistsState.hindiVideoList.length < 1){
+            ( async () => {
+                try{
+                    await getVideos();
+    
+                    playlistsDispatch(
+                        {
+                            type: "CATEGORIZE_VIDEOS",
+                            payload: {
+                                hindiVideos,
+                                vocabularyVideos,
+                                pronunciationVideos
+                            }
+                        }
+                    )
+                }
+                catch(error){
+                    console.log('Hindi - Error getting videos:', error)
+                }
+            })()
+        }
+
+    }, [getVideos, playlistsState.hindiVideoList]);
+
+    return (
+        <div>
+            <h3 className="textLg fontSemiBold mb8">English through Hindi <small className="textGray4">({playlistsState.hindiVideoList.length})</small></h3>
+                
+            <div className="displayGrid gridCols1 md:gridCols4 gridGap4 mb12">
+                {playlistsState.hindiVideoList.map(video => {
+                    return (
+                        <HomeVideoCard className="" key={video.id} video={video} />
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
