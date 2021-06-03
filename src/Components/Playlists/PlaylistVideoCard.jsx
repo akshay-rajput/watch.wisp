cc071c06772f701a93e7b1270826ace8f12983ed
Akshay <akshayrajput102@gmail.com>
2021-06-03 19:29:23 +0530

    style - playlistVideoCard - use path to determine style of card

--- src/Components/Playlists/PlaylistVideoCard.jsx
+++ src/Components/Playlists/PlaylistVideoCard.jsx
@@ -1,13 +1,16 @@
 import React, {useState} from 'react';
 import styles from './PlaylistVideoCard.module.css';
-import {MdVisibility, MdThumbUp, MdBookmark, MdBookmarkBorder, MdPlaylistAdd} from 'react-icons/md';
+import {MdVisibility, MdThumbUp, MdPlaylistAdd} from 'react-icons/md';
 import {convertNumberScale} from '../../utils';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import SaveVideoButton from '../common/SaveVideoButton';
 import PlaylistPopup from '../common/PlaylistPopup';
 
 export default function PlaylistVideoCard({video}) {
     const [isPlaylistPopupOpen, setIsPlaylistPopupOpen] = useState(false)
+
+    const {pathname} = useLocation();
+    let pathnameArray = pathname.split('/');
 
     function openPlaylistPopup(){
         setIsPlaylistPopupOpen(!isPlaylistPopupOpen);
@@ -19,20 +22,21 @@ export default function PlaylistVideoCard({video}) {
 
     
     return (
-        <div className={`${styles.playlist_video_card} gridColSpan4 md:gridColSpan1`}>
+        <div className={ pathnameArray[1] === "video" ? styles.playlist_video_card_horizontal : styles.playlist_video_card + ` gridColSpan4 md:gridColSpan1`}>
             <img src={video.snippet.thumbnailUrl} alt="video" className={`${styles.playlist_video_card_image}`} />
             
             <div className={`${styles.playlist_video_card_info} displayFlex flexCol`}>
+                
                 <div className="flexGrow">
                     <Link to={`/video/${video._id}`} className={`${styles.playlist_video_card_title} displayBlock md:mb2 fontNormal`}>
                         {video.snippet.name}
                     </Link>
                     <div className="playlist_video_card_stats displayFlex itemsCenter mt2">
                         <span className="textXs md:textSm mr4 displayFlex itemsCenter">
-                            <MdVisibility className="mr1" /> {convertNumberScale(video.views)}
+                            <MdVisibility className="mr1 textGray4" /> {convertNumberScale(video.views)}
                         </span>
                         <span className="textXs md:textSm displayFlex itemsCenter">
-                            <MdThumbUp className="mr1" /> {convertNumberScale(video.likes)}
+                            <MdThumbUp className="mr1 textGray4" /> {convertNumberScale(video.likes)}
                         </span>
                     </div>
                 </div>
