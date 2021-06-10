3ee196107621c7b1ec914c4b77144addf4cd247f
Akshay <akshayrajput102@gmail.com>
2021-06-10 11:59:15 +0530

    feat - userPlaylist - show skeleton for playlistvideos

--- src/Components/Playlists/UserPlaylist.jsx
+++ src/Components/Playlists/UserPlaylist.jsx
@@ -5,6 +5,7 @@ import {useAuth} from '../../Store/AuthContext';
 import PlaylistVideoCard from './PlaylistVideoCard';
 import {usePlaylists} from '../../Store/PlaylistsContext';
 import {Link} from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
 
 export default function UserPlaylist() {
     let {playlistId} = useParams();
@@ -81,62 +82,79 @@ export default function UserPlaylist() {
 
     return (
         <div>
-        {
-                authState.token && playlistData.videos?.length > 0 ?
-                <>
-                    <div className="displayFlex justifyBetween itemsCenter mb6">
-                        <h3 className="textLg fontSemiBold">{playlistData.name} <small className="textGray4">({playlistData.videos.length})</small></h3>
-                        <label className="textSm">
-                            Sort by
-                            <select name="sortby" id="sortby" onChange={sortPlaylist} className="p1 rounded ml2">
-                                <option value="views">Views</option>
-                                <option value="likes">Likes</option>
-                            </select>
-                        </label>
-                    </div>
-                    
-                    <div className="displayGrid gridCols4 gridGap4 mb8">
-                        {
-                            playlistData.videos.map(video => {
-                                return(
-                                    <PlaylistVideoCard video={video} key={video._id}/>
-                                )
-                            })
-                        }
-                    </div>
-                </>
-                :
-                authState.token && playlistData.videos?.length < 1 ?
-                <>
-                    <div className="">
-                        <h3 className="textLg fontSemiBold">{playlistData.name} <small className="textGray4">({playlistData.videos.length})</small></h3>
-                        <div className="displayFlex flexCol itemsCenter justifyCenter pt12 md:pt24">
-                            <h4 className="textLg fontSemiBold">Playlist is empty!</h4>
-                            <p className="textGray4 textCenter textSm mt2">
-                                When you add videos to your playlist they appear here. <br /> You can add videos to this playlist from "Home" page or a particular video page.
-                            </p>
-                            <br />
-                            <Link to={'/'}>View Videos</Link>
+        {   
+            !authState.token ?
+            <>
+                <div className="">
+                    <h3 className="textLg fontSemiBold">{playlistData.name}</h3>
+                    <div className="displayFlex flexCol itemsCenter justifyCenter pt12 md:pt24">
+                        <h4 className="textLg fontSemiBold">This playlist is private!</h4>
+                        <p className="textGray4 textCenter textSm mt2">
+                            You do not have permission to view this playlist. <br /> If you've created this playlist login, to view it.
+                        </p>
+                        <div className="displayFlex mt8 mb2">
+                            <Link to={'/login'}>Login</Link>
+                            <Link to={'/signup'} className="ml8">Signup</Link>
                         </div>
                     </div>
-                </>
-                :
-                <>
-                    <div className="">
-                        <h3 className="textLg fontSemiBold">{playlistData.name}</h3>
-                        <div className="displayFlex flexCol itemsCenter justifyCenter pt12 md:pt24">
-                            <h4 className="textLg fontSemiBold">This playlist is private!</h4>
-                            <p className="textGray4 textCenter textSm mt2">
-                                You do not have permission to view this playlist. <br /> If you've created this playlist login, to view it.
-                            </p>
-                            <div className="displayFlex mt8 mb2">
-                                <Link to={'/login'}>Login</Link>
-                                <Link to={'/signup'} className="ml8">Signup</Link>
+                </div>
+            </>
+            :
+            authState.token && playlistData.videos?.length > 0 ?
+            <>
+                <div className="displayFlex justifyBetween itemsCenter mb6">
+                    <h3 className="textLg fontSemiBold">{playlistData.name} <small className="textGray4">({playlistData.videos.length})</small></h3>
+                    <label className="textSm">
+                        Sort by
+                        <select name="sortby" id="sortby" onChange={sortPlaylist} className="p1 rounded ml2">
+                            <option value="views">Views</option>
+                            <option value="likes">Likes</option>
+                        </select>
+                    </label>
+                </div>
+                
+                <div className="displayGrid gridCols4 gridGap4 mb8">
+                    {
+                        playlistData.videos.map(video => {
+                            return(
+                                <PlaylistVideoCard video={video} key={video._id}/>
+                            )
+                        })
+                    }
+                </div>
+            </>
+            :
+            authState.token && playlistData.videos?.length < 1 ?
+            <>
+                <div className="">
+                    <h3 className="textLg fontSemiBold">{playlistData.name} <small className="textGray4">({playlistData.videos.length})</small></h3>
+                    <div className="displayFlex flexCol itemsCenter justifyCenter pt12 md:pt24">
+                        <h4 className="textLg fontSemiBold">Playlist is empty!</h4>
+                        <p className="textGray4 textCenter textSm mt2">
+                            When you add videos to your playlist they appear here. <br /> You can add videos to this playlist from "Home" page or a particular video page.
+                        </p>
+                        <br />
+                        <Link to={'/'}>View Videos</Link>
+                    </div>
+                </div>
+            </>
+            :
+            <div className="displayGrid gridCols4 gridGap4 mb8">
+                <h3 className="gridColSpan4 textLg fontSemiBold">
+                    <Skeleton style={{ width: '100%' }}/>
+                </h3>
+                {
+                    [...Array(4)].map((child, index) => {
+                        return(
+                            <div className="gridColSpan4 md:gridColSpan1" key={index}>
+                            <Skeleton style={{ width: '100%', height: "200px", }}/>
                             </div>
-                        </div>
-                    </div>
-                </>
-            }
+                        )
+                    })
+                }
+            </div>
+            
+        }
         </div>
     )
 }
