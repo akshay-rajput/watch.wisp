9197df8bd0d3cc673736ea8a71e99e698c61bd35
Akshay <akshayrajput102@gmail.com>
2021-06-10 12:02:05 +0530

    refactor - minor space related commit

--- src/Components/HomeVideoCard.jsx
+++ src/Components/HomeVideoCard.jsx
@@ -5,7 +5,6 @@ import {convertNumberScale, truncateText, convertDuration, checkExistanceInArray
 import PlaylistPopup from '../Components/common/PlaylistPopup';
 import SaveVideoButton from '../Components/common/SaveVideoButton';
 import {Link, useLocation} from 'react-router-dom';
-
 import useWindowDimensions from "../Hooks/useWindowDimensions";
 
 export default function HomeVideoCard({video}) {
@@ -24,38 +23,40 @@ export default function HomeVideoCard({video}) {
     }
 
     return (
-        <div className={pathname=='/' ? styles.video_card : styles.category_video_card}>
-            {/* <div className={`${styles.video_card_imagewrap}`}>
-                <img src={video.snippet.thumbnailUrl} alt="Video Thumbnail" title={video.snippet.name} className={`${styles.video_card_image}`} />
-                <span className={`${styles.video_card_duration} textSm`}>{convertDuration(video.duration)}</span>
-
-            </div> */}
-            <Link to={'/video/'+video._id} title={video.snippet.name} className={`${styles.video_card_imagewrap}`}>
-                <img src={video.snippet.thumbnailUrl} alt="Video Thumbnail" title={video.snippet.name} className={`${styles.video_card_image}`} />
-                <span className={`${styles.video_card_duration} textSm`}>{convertDuration(video.duration)}</span>
-            </Link>
-            <div className={`pl2 pr2 pb2`}>
-                <Link to={'/video/'+video._id} title={video.snippet.name} className={`${styles.video_card_title} cursorPointer fontNormal`}>
-                    {video.snippet.name}
+        <>
+            <div className={pathname=='/' ? styles.video_card : styles.category_video_card}>
+            
+                <Link to={'/video/'+video._id} title={video.snippet.name} className={`${styles.video_card_imagewrap}`}>
+                    <img src={video.snippet.thumbnailUrl} alt="Video Thumbnail" title={video.snippet.name} className={`${styles.video_card_image}`} />
+                    <span className={`${styles.video_card_duration} textSm`}>{convertDuration(video.duration)}</span>
                 </Link>
-                <span className="textXs textGray4">
-                    {width < 768 ? truncateText(video.channelTitle, 25) : truncateText(video.channelTitle, 40)} &bull; {convertNumberScale(video.views)} views
-                </span>
+                <div className={`pl2 pr2 pb2`}>
+                    <Link to={'/video/'+video._id} title={video.snippet.name} className={`${styles.video_card_title} cursorPointer fontNormal`}>
+                        {video.snippet.name}
+                    </Link>
+                    <span className="textXs textGray4">
+                        {width < 768 ? truncateText(video.channelTitle, 25) : truncateText(video.channelTitle, 40)} &bull; {convertNumberScale(video.views)} views
+                    </span>
 
-                {/* add to playlist */}
-                <div className={`${styles.video_card_button_group} mt2`}>
-                    
-                    <SaveVideoButton video={video} />
-                    
-                    <button onClick={openPlaylistPopup} className={`${styles.video_card_button}`} title={'Add to playlist'}>
-                        <MdPlaylistAdd />    
-                    </button>
+                    {/* add to playlist */}
+                    <div className={`${styles.video_card_button_group} mt2`}>
+                        
+                        <SaveVideoButton video={video} />
+                        
+                        <button onClick={openPlaylistPopup} className={`${styles.video_card_button}`} title={'Add to playlist'}>
+                            <MdPlaylistAdd />    
+                        </button>
+                    </div>
+
+                    {   isPlaylistPopupOpen &&
+                        <PlaylistPopup isOpen={isPlaylistPopupOpen} videoData={video} closePopup = {closePopup} />
+                    }
                 </div>
 
-                {   isPlaylistPopupOpen &&
-                    <PlaylistPopup isOpen={isPlaylistPopupOpen} videoData={video} closePopup = {closePopup} />
-                }
             </div>
-        </div>
+
+        
+            
+        </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from './Store/AuthContext';
 import { PlaylistsProvider } from './Store/PlaylistsContext';
-
+// import './Store/Interceptors';
 import App from './App'
 
 ReactDOM.render(
