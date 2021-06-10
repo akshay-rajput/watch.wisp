dd0032147786f3064fd335828596bfd47abd85f3
Akshay <akshayrajput102@gmail.com>
2021-06-10 11:56:06 +0530

    feat - homeVideoSlider - show skeletons while loading

--- src/Components/HomeVideoSlider.jsx
+++ src/Components/HomeVideoSlider.jsx
@@ -3,8 +3,9 @@ import {MdChevronRight} from 'react-icons/md';
 import {Link} from 'react-router-dom';
 import HomeVideoCard from './HomeVideoCard';
 import styles from './HomeVideoSlider.module.css';
+import Skeleton from 'react-loading-skeleton';
 
-export default function HomeVideoSlider({videoList, title}) {
+export default function HomeVideoSlider({videoList, title, loading}) {
     return (
         <div className="mb12">
             <div className="displayFlex justifyBetween itemsCenter pl2 pr2 mb2">
@@ -18,11 +19,21 @@ export default function HomeVideoSlider({videoList, title}) {
             </div>
             
             <div className={`${styles.video_slider}`}>
-                {videoList.map(video => {
-                    return (
-                        <HomeVideoCard className="" key={video.id} video={video} />
-                    )
-                })}
+                {
+                    !loading ?
+                    videoList.map(video => {
+                        return (
+                            <HomeVideoCard className="" key={video.id} video={video} />
+                        ) 
+                    }) 
+                    : 
+                    [...Array(6)].map((child, index) => {
+                        return(
+                            <Skeleton key={index} style={{ width: '300px', height: "250px", }}/>
+                        )
+                    })
+                }
+                
             </div>
             
         </div>
