07c5d608e75bd066a15d6d91b9bb9b69d48ce7f4
Akshay <akshayrajput102@gmail.com>
2021-06-11 14:56:50 +0530

    feat - about - add more info on page

--- src/Views/About.jsx
+++ src/Views/About.jsx
@@ -4,14 +4,20 @@ import {Link} from 'react-router-dom';
 
 export default function About() {
     let features = [
+        "State management (UseContext + UseReducer)",
         "JWT Authentication", "User Management", 
         "Create Playlist", "Add/Remove from playlist",
         "Save videos", "Search video",
         "Dynamic routing", "Handling empty states"
     ]
 
+    let enhancements = [
+        "Add notes for a video", "Use markdown to take notes",
+        "Notes page - showing all videos with notes", "Dark mode", "Change user avatar"
+    ]
+
     return (
-        <div className="md:ml4">
+        <div className="md:ml4 mb12">
             <h2 className="textMd md:textLg">About</h2>
             <p className="textGray4 lineHeightMd mt2 mb2">
                 Wisp is a video library app which shows curated youtube videos related to vocabulary and english speaking.
@@ -20,14 +26,29 @@ export default function About() {
                 This is a MERN stack app. More information about libraries used is on
                 <Link to="/acknowledgements" className="ml1 mr1 textBlue5 hover:textBlue6">acknowledgements</Link> page.
             </p>
-            <div className="textGray5 lineHeightMd mt2 mb2">
-                Some of the implemented features are:
+            <div className="textGray5 lineHeightMd mt3 mb3">
+                Some of the implemented <strong>features</strong> are:
                 <ul className="">
                     {
                         features.map(feature => {
                             return(
                                 <li key={feature} className="displayFlex itemsCenter">
                                     <MdKeyboardArrowRight /> {feature}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+
+            <div className="textGray5 lineHeightMd mt3 mb3">
+                Some of the <strong>enhancements</strong> which can be made down the line.
+                <ul className="">
+                    {
+                        enhancements.map(enhancement => {
+                            return(
+                                <li key={enhancement} className="displayFlex itemsCenter">
+                                    &bull; {enhancement}
                                 </li>
                             )
                         })
